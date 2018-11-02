45bf848863961e6697c57a1d001089757a3292f0
Eduardo Pierangeli <evp.pierangeli@gmail.com>
2018-11-02 11:01:35 -0300

    .

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from interfaceUsuario import InterfaceUsuario
 
 #InterfaceUsuario.inputsUsuario()
 
-InterfaceUsuario.inputsUsuarioComParametros("u", "n", "r", "a", 10, 80, 40, 50)
+InterfaceUsuario.inputsUsuarioComParametros("u", "n", "r", "a", 10, 80, 50, 80)
